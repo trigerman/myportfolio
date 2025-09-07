0d88159f7e3b0e5c90bee8f561c1729f0285d1ca
trigerman <61380761+trigerman@users.noreply.github.com>
2025-09-06 22:36:19 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// IMPORTANT: set base to your repo name when deploying to GitHub Pages.
 export default defineConfig({
   plugins: [react()],
-  base: '/myportfolio/'
+  base: '/myportfolio/',   // IMPORTANT: must match your repo name
 })
